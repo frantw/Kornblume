46fb7c2027347356cce36e4c16965b9447f9bd5b
Fran <wei840220@gmail.com>
2023-12-25 22:54:35 +0800

    Updates

---
+++ assets/pullsRecordStore-nR9f8V6z.js
@@ -0,0 +1,1 @@
+import{q as e}from"./index-XlC60ZFv.js";const a=e("pulls",{state:()=>({data:[]}),actions:{updatePullsRecord(t){this.data=[...t]},reset(){this.data=[]}},persist:!0});export{a as u};
--- assets/pullsRecordStore-wGsyS8ZY.js
+++
@@ -1,1 +0,0 @@
-import{q as e}from"./index-ybm2mukE.js";const a=e("pulls",{state:()=>({data:[]}),actions:{updatePullsRecord(t){this.data=[...t]},reset(){this.data=[]}},persist:!0});export{a as u};
